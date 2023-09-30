838338c3d0d2ac6cedf5ec5b152038fba52abddb
tusynkabert <138988098+tusynkabert@users.noreply.github.com>
2023-09-30 09:43:56 -0700

    Download dist

---
+++ dist/scripts/script.js
@@ -0,0 +1,7 @@
+/**
+ * Header
+ */
+document.querySelector('.burger').onclick = function (event) {
+    document.querySelector('.burger').classList.toggle('show')
+    document.querySelector('.nav').classList.toggle('show')
+}
